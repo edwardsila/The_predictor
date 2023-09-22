cf69357b4028fa540718fbee7d5a9c2b633f2689
Thugger <thugger@kali>
2023-09-22 10:27:01 +0300

    updated

---
+++ static/app.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	var myElement = document.getElementById('my-element');
+
+	myElement.addEventListener('click', function () {
+        alert('You clicked the element!');
+    });
+});
